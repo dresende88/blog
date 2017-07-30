889c9997860721868439d7b36f4cb671a1f80b3d
Daniel Resende <danielresende88@gmail.com>
2017-07-30 16:49:50 -0300

    Imprimindo ip para teste de loadbalancer

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel Noisss pranchaaaa caraiuuu
+                    Laravel {{$ip}}
                 </div>
 
                 <div class="links">
